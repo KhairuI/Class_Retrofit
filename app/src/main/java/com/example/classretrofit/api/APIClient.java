a4f495c1c493f00d962b3b9583bdf5b9a24802e0
Khairul Islam <khairul4166@gmail.com>
2021-01-03 20:49:42 +0600

    Get and Post

---
+++ app/src/main/java/com/example/classretrofit/api/APIClient.java
@@ -0,0 +1,27 @@
+package com.example.classretrofit.api;
+
+import com.example.classretrofit.model.Comment;
+import com.example.classretrofit.model.Post;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface APIClient {
+
+    @GET("posts")
+    Call<List<Post>> getPost(@Query("userId") int[] userId);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComment(@Path("id") int commentId);
+
+    // post request.....
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+}
+// posts/2/comments
